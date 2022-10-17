cef7cebf79d0463a2bca2eb264fed25bffcfc919
clicktoaddemail <71800552+clicktoaddemail@users.noreply.github.com>
2022-10-17 15:23:36 -0400

    Add files via upload

--- progRAM/modulate.c
+++ progRAM/modulate.c
@@ -1,14 +1,14 @@
 /*Source file for RAM modulation.
 CREATED: 10/13/22
-LAST MODIFIED: 10/14/22
+LAST MODIFIED: 10/17/22
 
 Changelog:
+10/17/22
+	- completed multithreading
 10/14/22
 	- refactoring
 	- added multithreaded modulation for an integer
 	- TODO: complete multithreading
-10/13/22
-	- created
 */
 
 #include "modulate.h"
@@ -16,17 +16,25 @@ Changelog:
 #include <string.h>
 #include <pthread.h>
 
+pthread_mutex_t perform_swaps = PTHREAD_MUTEX_INITIALIZER;
+
+
+unsigned char RAM_array1[MEGA];
+unsigned char RAM_array2[MEGA];
+
+
+
 //--- THREADED FUNCTIONS ---///
 void *oscillate(void *end_time) { //internal function that oscillates a specified chunk of memory for a set time
 	
-	time_t *bit_end_time = (time_t *)end_time;
-	unsigned char RAM_array1[block_size];
-	unsigned char RAM_array2[block_size];
+	time_t bit_end_time = *(time_t *)end_time;
 
-	while (time(NULL)*1000 < (*bit_end_time)) {
+	pthread_mutex_lock(&perform_swaps);
+	while (time(NULL)*1000 < bit_end_time) {
 		memcpy(RAM_array1,RAM_array2, block_size);
 		memcpy(RAM_array2,RAM_array1, block_size);
 	}
+	pthread_mutex_unlock(&perform_swaps);
 	pthread_exit(NULL);
 }
 
@@ -61,9 +69,11 @@ void multi_modulate_i(int bits, int cores) {
 	for (char i = 0;i<32;i++) {
 		bit_end_time = bit_end_time+time_per_bit_ms;
 		if ((bits>>i)&&0x00000001) { //only multithreads if there is a 1, to amplify the signal, might add delay....
+			pthread_mutex_lock(&perform_swaps);
 			for (int j=0;j<cores; j++) {
 				pthread_create(&thread_id, NULL, oscillate, (void *)&bit_end_time);
 			}
+			pthread_mutex_unlock(&perform_swaps); //release the threads
 		} else {
 			while (time(NULL)*1000<bit_end_time) {};
 		}
