0ef2d4cde92fae2dae93a4e07a1ad56f4ce6c901
clicktoaddemail <71800552+clicktoaddemail@users.noreply.github.com>
2022-10-31 08:23:42 -0400

    Transmission Adjustments

--- progRAM/main.c
+++ progRAM/main.c
@@ -1,9 +1,11 @@
 /* C program for pulsing a block of RAM to create some sort of EM response
 that can be received on an external device.
 CREATED: 10/4/22
-MODIFIED: 10/29/22
+MODIFIED: 10/31/22
 
 CHANGELOG:
+10/31/22
+	- made program loop indefinately
 10/29/22
 	- changed test vectors to a pattern that can be discerned on demodulation
 	- adjusted comments
@@ -22,15 +24,20 @@ CHANGELOG:
 //DRIVER CODE FOR RAM MODULATION
 int main(void) {
 	//32b over a single core
-	time_per_bit_ms = 250;
-	printf("Attempting to transmit 32b on a single core, est. %lims to complete.\n", 32*time_per_bit_ms);
-	int TestBits = 0xF0A5C3F0; //0b1111 0000 1010 0101 1100 0011 1111 0000
-	time_t start, end;
-	start = time(NULL) * 1000;
-	modulate_i(TestBits);
-	end = (time(NULL) * 1000) - start;	
 
-	printf("Done transmitting. Took %ldms.\n", end); //end should equal 32*time_per_bit_ms
+	while (1) {
+		printf("Attempting to transmit 32b on a single core, est. %lims to complete.\n", 32*time_per_bit_ms);
+		int TestBits = 0xF0A5C3F0; //0b1111 0000 1010 0101 1100 0011 1111 0000
+		time_t start, end;
+		start = time(NULL) * 1000;
+		modulate_i(TestBits);
+		end = (time(NULL) * 1000) - start;	
+
+		printf("Done transmitting. Took %ldms.\n", end); //end should equal 32*time_per_bit_ms
+		//wait 1000ms between transmissions
+		printf("Waiting...\n");
+		while ((time(NULL) * 1000) < end+1000) {}
+	}
 	return 0;
 }
 
